1abb46a3161fdfac897d358b6fc920f4dc4c023c
AHarnish <aaronjharnish@gmail.com>
2023-12-21 17:29:35 -0500

    white-car moves right

---
+++ Assets/MoveRight.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRight : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody.velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
