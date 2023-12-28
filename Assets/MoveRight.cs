2c9004e6fbad715385a78f7e8b2db2a3bd9dbabd
AHarnish <aaronjharnish@gmail.com>
2023-12-28 18:41:15 -0500

    Accounted for collider size in ray calculations

--- Assets/MoveRight.cs
+++ Assets/MoveRight.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class MoveRight : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
+    public Collider2D myCollider;
     public float speed = 10.0f;
 
     public LayerMask layerMask;
@@ -19,16 +21,26 @@ public class MoveRight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Find distance to next car
+
         RaycastHit2D hit = Physics2D.Raycast(myRigidBody.position, myRigidBody.velocity, 100f, layerMask);
         
         if (hit)
         {
-            Debug.Log(hit.rigidbody.velocity);
+
+            // Determine if current velocity, acceleration, and distance are okay
+            float myVelocity = myRigidBody.velocity.magnitude;
+            float hitVelocity = hit.rigidbody.velocity.magnitude;
+
+            float distance = hit.distance - myCollider.bounds.size.x / 2f;
+
+            Debug.Log(hit.distance);
+
+
         }
 
-        // Find distance to next car
 
-        // Determine if current velocity, acceleration, and distance are okay
+
         // Update acceleration if not
         if (Math.Abs(transform.position.x) > 70)
         {
