89f91baf2c3fd287dcd03109fc3fdf2d5cccbd6b
AHarnish <aaronjharnish@gmail.com>
2023-12-28 16:39:22 -0500

    alligned car spawner

--- Assets/SpawnCar.cs
+++ Assets/SpawnCar.cs
@@ -11,7 +11,7 @@ public class SpawnCar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+//        Ray ray = new Ray(transform.position, transform.forward);
     }
 
     // Update is called once per frame
