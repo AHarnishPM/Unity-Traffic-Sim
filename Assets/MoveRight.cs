5e4a40c26280a7ccc816e5b9f45005448dcb7520
AHarnish <aaronjharnish@gmail.com>
2023-12-28 18:18:44 -0500

    Added raycast from car objects

--- Assets/MoveRight.cs
+++ Assets/MoveRight.cs
@@ -8,6 +8,8 @@ public class MoveRight : MonoBehaviour
     public Rigidbody2D myRigidBody;
     public float speed = 10.0f;
 
+    public LayerMask layerMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,15 @@ public class MoveRight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit2D hit = Physics2D.Raycast(myRigidBody.position, myRigidBody.velocity, 100f, layerMask);
+        
+        if (hit)
+        {
+            Debug.Log(hit.rigidbody.velocity);
+        }
+
         // Find distance to next car
+
         // Determine if current velocity, acceleration, and distance are okay
         // Update acceleration if not
         if (Math.Abs(transform.position.x) > 70)
