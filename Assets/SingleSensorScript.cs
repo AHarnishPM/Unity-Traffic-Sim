064243b344047dd7437b5b2c6d5f4e2ece9b93bf
AHarnish <aaronjharnish@gmail.com>
2024-01-02 14:06:57 -0500

    added view distance var for single sensor

--- Assets/SingleSensorScript.cs
+++ Assets/SingleSensorScript.cs
@@ -6,11 +6,12 @@ public class SingleSensorScript : MonoBehaviour
 {
     public RaycastHit2D hit;
     public LayerMask mask;
+    public float viewDistance = 1000f;
 
     // Start is called before the first frame update
     void Start()
     {
-        hit = Physics2D.Raycast(transform.position, transform.rotation * Vector2.right, 1000f, mask);
+        hit = Physics2D.Raycast(transform.position, transform.rotation * Vector2.right, viewDistance, mask);
     }
 
     // Update is called once per frame
