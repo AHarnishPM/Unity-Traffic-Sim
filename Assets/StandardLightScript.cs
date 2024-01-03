a8c946947df36984dd7f23638d9513e1cb9813c4
AHarnish <aaronjharnish@gmail.com>
2024-01-03 11:53:20 -0500

    Added orientations matrix, assigned components in SL prefab

--- Assets/StandardLightScript.cs
+++ Assets/StandardLightScript.cs
@@ -18,6 +18,20 @@ public class StandardLightScript : MonoBehaviour
     public LightBarrierScript leftScript;
     public LightBarrierScript rightScript;
     public LightBarrierScript bottomScript;
+
+    // 0 = red, 1=yellow, 2=green
+    // Values in this order: top, left, right, bottom
+    // When transitioning, must approach first orientation where all are red first
+    public int[,] orientations =
+    {
+        {0, 0, 0, 0 }, // All blocked
+        {0, 2, 2, 0 }, // Horizontal free, vertical blocked
+        {2, 0, 0, 2 }, // Vertical free, horizontal blocked
+        {2, 0, 0, 0 }, // Top open
+        {0, 2, 0, 0 }, // Left open
+        {0, 0, 2, 0 }, // Right open
+        {0, 0, 0, 2 }  // Bottom open
+    };
     // Start is called before the first frame update
     void Start()
     {
