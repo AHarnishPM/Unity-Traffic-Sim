31167938401c2dbd41af018e7b515fcda217d170
AHarnish <aaronjharnish@gmail.com>
2024-01-10 14:49:19 -0500

    Option to visualize view distance of traffic sensors in each direction

--- Assets/MultipleSensorScript.cs
+++ Assets/MultipleSensorScript.cs
@@ -6,8 +6,9 @@ public class MultipleSensorScript : MonoBehaviour
 {
     public List<RaycastHit2D> hitList = new();
     public ContactFilter2D contactFilter;
-    public float viewDistance = 1000f;
+    public float viewDistance = 100f;
     public int numHits;
+    public bool visualizeViewDistance;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,10 @@ public class MultipleSensorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (visualizeViewDistance)
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.rotation * Vector2.right * viewDistance);
+        }
         numHits = Physics2D.Raycast(transform.position, transform.rotation * Vector2.right, contactFilter, hitList, viewDistance);
     }
 }
